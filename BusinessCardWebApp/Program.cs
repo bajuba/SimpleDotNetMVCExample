f79b47d36bcbfcd5bfdd94bf8c70709bec16c2bf
Caleb Lane <clane11815@lakelandcollege.edu>
2021-08-04 10:11:01 -0500

    add html, javascript, c#, and css files

--- BusinessCardWebApp/Program.cs
+++ BusinessCardWebApp/Program.cs
@@ -1,11 +1,13 @@
 using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using QRCoder;
 
 namespace BusinessCardWebApp
 {
@@ -14,6 +16,12 @@ namespace BusinessCardWebApp
         public static void Main(string[] args)
         {
             CreateHostBuilder(args).Build().Run();
+            QRCodeGenerator qrGenerator = new QRCodeGenerator();
+            QRCodeData qrCodeData = qrGenerator.CreateQrCode("QR", QRCodeGenerator.ECCLevel.Q);
+            QRCode qrCode = new QRCode(qrCodeData);
+            Bitmap qrCodeImage = qrCode.GetGraphic(20);
+
+            qrCodeImage.Save(@"C:\Users\caleb\source\repos\BusinessCard\BusinessQR.bmp");
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
---
+++ BusinessCardWebApp/wwwroot/js/script.js
@@ -0,0 +1,23 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+// JavaScript source code
+
+function generateCard() {
+
+    var nameField = document.getElementById("name");
+    var jobTitleField = document.getElementById("job-title");
+    var businessField = document.getElementById("business");
+    var addressField = document.getElementById("address");
+    var emailField = document.getElementById("email");
+    var phoneNumField = document.getElementById("phone-number");
+
+    document.getElementById("name-card").innerHTML = nameField.value;
+    document.getElementById("job-title-card").innerHTML = jobTitleField.value;
+    document.getElementById("business-card").innerHTML = businessField.value;
+    document.getElementById("address-card").innerHTML = "Address: " + addressField.value;
+    document.getElementById("email-card").innerHTML = "Email: " + emailField.value;
+    document.getElementById("phone-number-card").innerHTML = "Phone #: " + phoneNumField.value;
+
+}
